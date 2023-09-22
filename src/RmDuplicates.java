96c63b2ab9dd5723ef4ae2ce9ce73a4da31adbb4
Beno <circiubeni@gmail.com>
2023-09-22 16:54:41 +0300

    Changed the list type from ArrayList to Set (for a cleaner code)

--- src/RmDuplicates.java
+++ src/RmDuplicates.java
@@ -1,11 +1,12 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.lang.System;
+import java.util.Set;
 
 
 public class RmDuplicates {
-    private ArrayList<String> lines = new ArrayList<>();
+    private Set<String> lines = new HashSet<>();
     private Scanner scan = new Scanner(System.in);
     private String filename;
     private int duplicates;
@@ -19,20 +20,19 @@ public class RmDuplicates {
         RW = new FileReadWrite(this.filename,this.path,"DuplicatesRemoved.txt");
         RW.FileReadWrite();
         if(RW.scanfile != null)
-        while(RW.scanfile.hasNextLine())
-        {
-            String data = RW.scanfile.nextLine();
-            if(this.lines.contains(data) || data.equals("")) //check if line already exist in the Array
-                this.duplicates++;
-            else {
-                this.lines.add(data);
-                try{
-                RW.FileOUT.write(data+"\n");} //Write to file if it isn't a duplicate
-                catch (IOException e){
-                    System.out.println(e.getMessage());
+            while(RW.scanfile.hasNextLine())
+            {
+                String data = RW.scanfile.nextLine();
+                if(!this.lines.add(data) || data.equals("")) //check if line already exist in the Array
+                    this.duplicates++;
+                else {
+                    try{
+                        RW.FileOUT.write(data+"\n");} //Write to file if it isn't a duplicate
+                    catch (IOException e){
+                        System.out.println(e.getMessage());
+                    }
                 }
             }
-        }
         else {
             return;
         }
